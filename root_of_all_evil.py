7bbb8733b9aaf4b5195c8fd5caf73accb0881dfe
taheld77 <taheld77@gmail.com>
2025-06-25 18:58:51 +0300

    done!

--- root_of_all_evil.py
+++ root_of_all_evil.py
@@ -1,11 +1,11 @@
 def sqrt(number):
-    if type(number) != float:
-        raise TypeError("The input is not an integer")
+    if number < 0:
+        raise ValueError("Number cannot be negative")
     number = number ** 0.5
     return number
 
 def main():
-  print(sqrt(float(input("Enter a number: "))))
+    print(sqrt(float(input("Enter a number: "))))
 
 if __name__ == "__main__":
     main()
